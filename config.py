415103a7b4caa1fef08e4f4f9a07d8080209e27b
Kavyasri Rejeti <your-email@example.com>
2025-05-19 11:52:31 +0530

    Updated feature/fixed bug

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
-MAIL_USERNAME = 'your_email@gmail.com'       # replace with your Gmail
-MAIL_PASSWORD = 'your_app_password'          # generate from https://myaccount.google.com/apppasswords
+MAIL_USERNAME = 'kavyarejeti01@gmail.com'       # replace with your Gmail
+MAIL_PASSWORD = 'Alphakavya@123'          # generate from https://myaccount.google.com/apppasswords
